009452df5b7e07bdae207e6d306954ad3a592228
Estif7 <estifanosedejene7@gmail.com>
2026-07-21 01:43:10 -0900

    refactor(course): rename Capacity to MaxCapacity and align Code column length

--- Data/Configurations/CourseConfiguration.cs
+++ Data/Configurations/CourseConfiguration.cs
@@ -12,7 +12,7 @@ public class CourseConfiguration : IEntityTypeConfiguration<Course>
 
         builder.Property(c => c.Code)
             .IsRequired()
-            .HasMaxLength(20);
+            .HasMaxLength(10);
 
         builder.HasIndex(c => c.Code)
             .IsUnique();
@@ -20,5 +20,10 @@ public class CourseConfiguration : IEntityTypeConfiguration<Course>
         builder.Property(c => c.Title)
             .IsRequired()
             .HasMaxLength(200);
+
+        builder.HasMany(c => c.Enrollments)
+            .WithOne(e => e.Course)
+            .HasForeignKey(e => e.CourseId);
+
     }
 }

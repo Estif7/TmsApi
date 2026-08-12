8732102528847117d2dfa8def4ba09ebab39831e
Estif7 <estifanosedejene7@gmail.com>
2026-08-11 22:13:51 -0900

    feat: implement centralized EnrollmentStore with NgRx Signals

--- tms-client/src/app/app.routes.ts
+++ tms-client/src/app/app.routes.ts
@@ -22,5 +22,12 @@ export const routes: Routes = [
         loadComponent: () =>
             import('./features/enrollment-form/enrollment-form')
                 .then(m => m.EnrollmentForm)
-    }
-];
+    },
+    {
+        path: 'enrollments',
+        loadComponent: () =>
+        import('./features/enrollment-list/enrollment-list')
+            .then((m) => m.EnrollmentList),
+    },
+    { path: '', redirectTo: 'enrollments', pathMatch: 'full' },
+    ];
